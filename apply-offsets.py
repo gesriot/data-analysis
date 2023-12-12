6edc5ffd165e361a7120f2fed2c79852e9141c86
gesriot <79553594+gesriot@users.noreply.github.com>
2023-12-12 22:26:09 +0300

    Universal algorithm

--- apply-offsets.py
+++ apply-offsets.py
@@ -1,6 +1,16 @@
-def process_file(path):
+def generate_values(a, b):
+    if not (0.1 <= a <= 0.9) or not (1 <= b <= 9):
+        raise ValueError("a должно быть в [0.1; 0.9], b - в диапазоне [1; 9]")
+    ls = list(range(10))
+    a_ = int(a*10) - 1
+    for i in range(11 - b):
+        ls.remove((a_ + i) % 10)
+    ls.append(a_)
+    return ls
+
+def process_file(path, offset, discard_values):
     """
-    Значения 0.8..1.4 в последнем столбце преобразуются в 0.0..0.6 с корректным отображением целой части
+    (10 - discard_values) значений начиная с offset
     """
     with open(path, 'r') as f:
         result = []
@@ -12,8 +22,11 @@ def process_file(path):
             encoder = int(encoder)
             fraction = int(fraction)
 
-            if fraction not in (5, 6, 7):
-                value_ = float(value) - 0.8
+            discard_range = generate_values(offset, discard_values)
+
+            if fraction not in discard_range:
+                value_ = float(value) - offset
+                
                 if value_ < 0:
                     value_ = value_ + 368
                 value = f'{value_:.1f}'
@@ -25,7 +38,7 @@ def process_file(path):
             f.write(line + '\n')
 
 def main():
-    process_file("file_cut.csv")
+    process_file("new-57.csv", 0.8, 3)
 
 if __name__ == "__main__":
     main()
